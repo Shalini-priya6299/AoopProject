6f87547f1d4350d6e75e3c80de5f14a8b70b0a3b
Shalini-priya6299 <shalinipriya9155@gmail.com>
2026-04-03 18:24:06 +0530

    Tokenizer implemented with indentation, multi-word operators, and test cases

--- test/tokenizer/TokenizerTest.java
+++ test/tokenizer/TokenizerTest.java
@@ -1,11 +1,9 @@
-package test;
-
-import tokenizer.*;
-import java.util.*;
+package tokenizer;
+import java.util.List;
 
 public class TokenizerTest {
     public static void main(String[] args){
-        String input = "repeat 2 times\n    say x\nsay done";
+        String input = "x is greater\nthan y";
         Tokenizer t1 = new Tokenizer(input);
         List<Token> tokens = t1.tokenize();
         for(Token t: tokens){
